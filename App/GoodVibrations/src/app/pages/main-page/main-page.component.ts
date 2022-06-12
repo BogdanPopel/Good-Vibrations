fe3f26d3cb6d3368d16139ca442570b7c980a5c7
rimihai2001 <rimihai2001@gmail.com>
2022-06-12 16:07:16 +0300

    Locations+Recommandation Pages Backend Done

--- App/GoodVibrations/src/app/pages/main-page/main-page.component.ts
+++ App/GoodVibrations/src/app/pages/main-page/main-page.component.ts
@@ -1,21 +1,90 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFireDatabase,
+  AngularFireDatabaseModule,
+} from '@angular/fire/compat/database';
+import 'firebase/database';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent implements OnInit {
+  public locationsList: Location[] | undefined;
+  public _id!: number;
+  public _photoUrl: string | undefined;
+  public _locationName: string | undefined;
+  public _locationAddress: string | undefined;
+  public _musicGenres: string[] | undefined;
+  public _mg: string | undefined;
+  public _mapsUrl: string | undefined;
+  constructor(public afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+    //this.db.object('locations/0').set({id: 1, photoUrl:"https://www.restocracy.ro/wp-content/uploads/2017/06/Quantic-Club-pub-si-club-in-Grozavesti-13.jpg", locationName:"Quantic Pub", address:"Șoseaua Grozăvești 82, București 060752", musicGenres:["Rock", "Metal"], mapsUrl:"https://goo.gl/maps/KxqxEgheoXKQn7Eh6"});
+    //console.log('db');
+    this.getStarted();
+  }
 
-  constructor(public afAuth: AngularFireAuth) { }
+  ngOnInit(): void {}
 
-    ngOnInit(): void {
-    }
+  logout(): void {
+    this.afAuth.signOut();
+  }
 
-    logout(): void {
-        this.afAuth.signOut();
-    }
+  async getStarted() {
+    //console.log("GetStarted");
+    var locations: Location[];
+    locations = [];
+    await this.getLocations().then((value) => {
+      locations = value as Location[];
+    });
 
+    this.locationsList = locations;
+    //console.log(this.locationsList);
+    this._id = this.locationsList.length + 1;
+  }
 
+  getLocations() {
+    //console.log("getLocations");
+    return new Promise((resolve, reject) => {
+      this.db
+        .list('locations')
+        .valueChanges()
+        .subscribe((value) => {
+          resolve(value);
+        });
+    });
+  }
+
+  async addLocation() {
+    this._musicGenres = this._mg?.split(',');
+    var data = {
+      id: this._id,
+      photoUrl: this._photoUrl,
+      locationName: this._locationName,
+      musicGenres: this._musicGenres,
+      mapsUrl: this._mapsUrl,
+      locationAddress : this._locationAddress
+    };
+    await this.db.object('locations/' + (this._id - 1)).set(data);
+    await this.getStarted();
+    this.clearFields();
+  }
+
+  clearFields() {
+    this._locationName = '';
+    this._mapsUrl = '';
+    this._mg = '';
+    this._photoUrl = '';
+  }
+}
+
+class Location {
+  id: number | undefined;
+  photoUrl: string | undefined;
+  locationName: string | undefined;
+  address: string | undefined;
+  musicGenres: string[] | undefined;
+  mapsUrl: string | undefined;
 }
--- App/GoodVibrations/src/app/pages/recommandation-page/recommandation-page.component.ts
+++ App/GoodVibrations/src/app/pages/recommandation-page/recommandation-page.component.ts
@@ -1,21 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-recommandation-page',
   templateUrl: './recommandation-page.component.html',
-  styleUrls: ['./recommandation-page.component.css']
+  styleUrls: ['./recommandation-page.component.css'],
 })
 export class RecommandationPageComponent implements OnInit {
+  public locationsList!: Location[];
+  public locationsListLucky!: Location[];
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+    this.getStarted();
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {}
 
-    logout(): void {
-        this.afAuth.signOut();
-    }
+  async getStarted() {
+    var locations: Location[];
+    locations = [];
+    await this.getLocations().then((value) => {
+      locations = value as Location[];
+    });
 
+    this.locationsList = locations;
 
+    var index = Math.floor(Math.random() * this.locationsList.length);
+
+    this.locationsListLucky = [this.locationsList[index]];
+    console.log(this.locationsListLucky, index);
+  }
+
+  getLocations() {
+    return new Promise((resolve, reject) => {
+      this.db
+        .list('locations')
+        .valueChanges()
+        .subscribe((value) => {
+          resolve(value);
+        });
+    });
+  }
+
+  logout(): void {
+    this.afAuth.signOut();
+  }
+}
+
+class Location {
+  id: number | undefined;
+  photoUrl: string | undefined;
+  locationName: string | undefined;
+  address: string | undefined;
+  musicGenres: string[] | undefined;
+  mapsUrl: string | undefined;
 }
